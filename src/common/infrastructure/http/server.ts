69cbaa98a80172f9badc1cd89cd3f0eb9863b01f
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-09 17:52:41 -0300

    Correção de identação

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,8 +1,14 @@
 import { env } from '../env'
+import { dataSource } from '../typeorm'
 import { app } from './app'
 
-app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT}! 🏆`)
-  console.log(`API docs available GET: http://localhost:${env.PORT}/docs 📚`) 
-})
 
+dataSource.initialize().then(() => {
+  app.listen(env.PORT, () => {
+    console.log(`Server running on port ${env.PORT}! 🏆`)
+    console.log(`API docs available GET: http://localhost:${env.PORT}/docs 📚`) 
+  })
+}).catch((error) => {
+  console.error('❌ Error during Data Source initialization:', error)
+  throw new Error('Error during Data Source initialization')
+})
