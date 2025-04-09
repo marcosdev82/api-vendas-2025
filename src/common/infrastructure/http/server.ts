d10142de973ca04f6f500691982e1cdee918c85d
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-09 17:18:16 -0300

    Configuração de conexão env

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,8 +1,6 @@
 import { env } from '../env'
 import { app } from './app'
 
-
-
 app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT}! 🏆`)
   console.log(`API docs available GET: http://localhost:${env.PORT}/docs 📚`) 
