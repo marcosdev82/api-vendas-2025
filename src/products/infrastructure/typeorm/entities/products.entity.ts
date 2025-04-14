9f7091a998343e1ce460a95e22bea46d6f127b10
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-14 13:15:32 -0300

    Estrutura da entidade

---
+++ src/products/infrastructure/typeorm/entities/products.entity.ts
@@ -0,0 +1,23 @@
+import { ProductModel } from '@/products/models/products.model';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('products')
+export class Product implements ProductModel {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @Column({name: 'created_at'})
+  created_at: Date;
+
+  @Column({name: 'updated_at'})
+  updated_at: Date;
+}
