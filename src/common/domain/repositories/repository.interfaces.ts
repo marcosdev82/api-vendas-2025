8577087e58d0071de96a02b4e1b63b363646ad43
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-16 13:21:08 -0300

    Tipagem de entrada e saida do search

---
+++ src/common/domain/repositories/repository.interfaces.ts
@@ -0,0 +1,25 @@
+export type SearchInput = {
+  page?: number
+  per_page?: number
+  sort?: string | null 
+  sort_dir?: string | null
+}
+
+export type SearchOutput<Model> = {
+  items: Model[]
+  per_page: number
+  total: number
+  current_page: number
+  sort: string | null 
+  sort_dir: string | null
+  filter: string | null
+}
+
+export interface RepositoryInterface<Model, CreateProps> {
+  create(props: CreateProps): Model
+  insert(model: Model): Promise<Model>
+  findById(id: string): Promise<Model | null>
+  update(model: Model): Promise<Model>
+  delete(id: string): Promise<void>
+  search(props: SearchInput): Promise<SearchOutput<Model>>  
+} 
