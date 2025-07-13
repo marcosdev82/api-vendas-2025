3cba37fc229a45d2671bce925d304653b9a446f6
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-13 12:44:05 -0300

    Teste paginate e sort

--- src/common/domain/repositories/in-memory.repository.spec.ts
+++ src/common/domain/repositories/in-memory.repository.spec.ts
@@ -242,5 +242,53 @@ describe('InMemoryRepository unit tests', () => {
         filter: 'test',
       })
     })
+
+    it('should apply paginate and sort', async () => { 
+      const items = [
+        { id: randomUUID(), name: 'b', price: 10, created_at, updated_at },
+        { id: randomUUID(), name: 'a', price: 20, created_at, updated_at },
+        { id: randomUUID(), name: 'd', price: 30, created_at, updated_at },
+        { id: randomUUID(), name: 'e', price: 10, created_at, updated_at },
+        { id: randomUUID(), name: 'c', price: 10, created_at, updated_at },
+      ]
+
+      sut.items = items
+
+      let result = await sut.search({
+        page: 1,
+        per_page: 2,
+        sort: 'name',
+        sort_dir: 'asc',
+        filter: null,
+      });
+      
+      expect(result).toStrictEqual({
+        items: [items[1], items[0]],
+        total: 5,
+        current_page: 1,
+        per_page: 2,
+        sort: 'name',
+        sort_dir: 'asc',
+        filter: null,
+      })
+
+      result = await sut.search({
+        page: 2,
+        per_page: 2,
+        sort: 'name',
+        sort_dir: 'asc',
+        filter: null,
+      });
+      
+      expect(result).toStrictEqual({
+        items: [items[4], items[2]],
+        total: 5,
+        current_page: 2,
+        per_page: 2,
+        sort: 'name',
+        sort_dir: 'asc',
+        filter: null,
+      })
+    }) 
   })
 })
