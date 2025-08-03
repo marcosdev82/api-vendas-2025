4c661b35d39ec0fb19744fb7e885137a706de84e
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-03 12:29:27 -0300

    Product in memory repository

--- src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
+++ src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
@@ -1,5 +1,6 @@
 import { NotFoundError } from "@/common/domain/errors/not-found-error"
 import { ProductsInMemoryRepository } from "./products-in-memory.repository"
+import { ProductDataBuilder } from "../testing/helpers/products-data-builder"
 
 describe('ProductsInMemoryRepository unit tests', () => {
   let sut: ProductsInMemoryRepository
@@ -14,6 +15,15 @@ describe('ProductsInMemoryRepository unit tests', () => {
       await expect(() => sut.findByName ('fake_name')).rejects.toThrow(
         new NotFoundError(`Product not found using name fake_name`)
       )
+
+      await expect(() => sut.findByName ('fake_name')).rejects.toBeInstanceOf(NotFoundError)
+    })
+
+    it('should find product by name', async () => {
+      const data = ProductDataBuilder({ name: 'Curso nodejs'})
+      sut.items.push(data)
+      const result = await sut.findByName('Curso nodejs')
+      expect(result).toEqual(data)
     })
   })
 })
