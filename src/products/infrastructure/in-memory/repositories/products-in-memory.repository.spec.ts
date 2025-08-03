7ada448fba0f7121421c5f3c8a4643f8c90cba2c
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-03 12:53:07 -0300

    conflict name

--- src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
+++ src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
@@ -13,11 +13,8 @@ describe('ProductsInMemoryRepository unit tests', () => {
 
   describe('findByName', () => {
     it('should throw error when product not found', async () => {
-      const data = ProductDataBuilder({ name: 'Curso nodejs'})
-      
-
-      await expect(() => sut.conflictingName('Curso nodejs')).rejects.toThrow(
-        new ConflictError(`Product not found using name fake_name`)
+      await expect(() => sut.findByName ('fake_name')).rejects.toThrow(
+        new NotFoundError(`Product not found using name fake_name`)
       )
 
       await expect(() => sut.findByName ('fake_name')).rejects.toBeInstanceOf(NotFoundError)
@@ -32,21 +29,21 @@ describe('ProductsInMemoryRepository unit tests', () => {
     
   })
 
-  describe('findByName', () => {
-    it('should throw error when product found', async () => {
-      await expect(() => sut.findByName ('fake_name')).rejects.toThrow(
-        new NotFoundError(`Product not found using name fake_name`)
-      )
+  describe('conflictingName', () => {
+      it('should throw error when product not found', async () => {
+        const data = ProductDataBuilder({ name: 'Curso nodejs' })
+        sut.items.push(data)
 
-      await expect(() => sut.findByName ('fake_name')).rejects.toBeInstanceOf(NotFoundError)
-    })
+        await expect(() => sut.conflictingName('Curso nodejs')).rejects.toThrow(
+          new ConflictError(`Name already used on another product: Curso nodejs`)
+        )
 
-    // it('should find product by name', async () => {
-    //   const data = ProductDataBuilder({ name: 'Curso nodejs'})
-    //   sut.items.push(data)
-    //   const result = await sut.findByName('Curso nodejs')
-    //   expect(result).toEqual(data)
-    // })
-    
+        await expect(() => sut.conflictingName('Curso nodejs')).rejects.toBeInstanceOf(ConflictError)
+      })
+
+      it('should not find product by name', async () => {
+        expect.assertions(0)
+        await sut.conflictingName('Curso nodejs')
+      })
   })
 })
