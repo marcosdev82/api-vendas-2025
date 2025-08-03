3c5ff10b60099b3a6bc037db6c35016da26885c5
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-03 12:42:16 -0300

    ajuste conflict name

--- src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
+++ src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
@@ -1,6 +1,7 @@
 import { NotFoundError } from "@/common/domain/errors/not-found-error"
 import { ProductsInMemoryRepository } from "./products-in-memory.repository"
 import { ProductDataBuilder } from "../testing/helpers/products-data-builder"
+import { ConflictError } from "@/common/domain/errors/not-found-conflict-error"
 
 describe('ProductsInMemoryRepository unit tests', () => {
   let sut: ProductsInMemoryRepository
@@ -12,8 +13,11 @@ describe('ProductsInMemoryRepository unit tests', () => {
 
   describe('findByName', () => {
     it('should throw error when product not found', async () => {
-      await expect(() => sut.findByName ('fake_name')).rejects.toThrow(
-        new NotFoundError(`Product not found using name fake_name`)
+      const data = ProductDataBuilder({ name: 'Curso nodejs'})
+      
+
+      await expect(() => sut.conflictingName('Curso nodejs')).rejects.toThrow(
+        new ConflictError(`Product not found using name fake_name`)
       )
 
       await expect(() => sut.findByName ('fake_name')).rejects.toBeInstanceOf(NotFoundError)
@@ -25,5 +29,24 @@ describe('ProductsInMemoryRepository unit tests', () => {
       const result = await sut.findByName('Curso nodejs')
       expect(result).toEqual(data)
     })
+    
+  })
+
+  describe('findByName', () => {
+    it('should throw error when product found', async () => {
+      await expect(() => sut.findByName ('fake_name')).rejects.toThrow(
+        new NotFoundError(`Product not found using name fake_name`)
+      )
+
+      await expect(() => sut.findByName ('fake_name')).rejects.toBeInstanceOf(NotFoundError)
+    })
+
+    // it('should find product by name', async () => {
+    //   const data = ProductDataBuilder({ name: 'Curso nodejs'})
+    //   sut.items.push(data)
+    //   const result = await sut.findByName('Curso nodejs')
+    //   expect(result).toEqual(data)
+    // })
+    
   })
 })
