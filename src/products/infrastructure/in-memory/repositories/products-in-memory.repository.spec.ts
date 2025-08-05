d5e93d2806b101c2d2a81184b00c884037971243
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-05 13:23:49 -0300

    Apply sort

--- src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
+++ src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
@@ -74,4 +74,34 @@ describe('ProductsInMemoryRepository unit tests', () => {
         spyFilterMethod.mockRestore() // restaura o método original após o teste
       })
   })
+
+  describe('applySort', () => {
+    it('should sort items by created_at when sort param is null', async () => {
+        const created_at = new Date()
+        const items = [
+          ProductDataBuilder({ name: 'c', created_at: created_at }),
+          ProductDataBuilder({
+            name: 'a',
+            created_at: new Date(created_at.getTime() + 100),
+          }),
+          ProductDataBuilder({
+            name: 'b',
+            created_at: new Date(created_at.getTime() + 200),
+          }),
+        ]
+        sut.items.push(...items)
+        const result = await sut['applySort'](sut.items, null, null)
+        expect(result).toStrictEqual([items[2], items[1], items[0]])
+      })
+      it('should sort items by name field', async () => {
+        const items = [
+          ProductDataBuilder({ name: 'c' }),
+          ProductDataBuilder({ name: 'a' }),
+          ProductDataBuilder({ name: 'b' }),
+        ]
+        sut.items.push(...items)
+        const result = await sut['applySort'](sut.items, 'name', 'desc')
+        expect(result).toStrictEqual([items[0], items[2], items[1]])
+      })
+    })
 })
