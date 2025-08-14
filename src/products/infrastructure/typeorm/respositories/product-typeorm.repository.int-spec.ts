892a7c6bad67a2bdb2acb9af4663b5df2632ff25
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-14 13:05:28 -0300

    Teste de integração

---
+++ src/products/infrastructure/typeorm/respositories/product-typeorm.repository.int-spec.ts
@@ -0,0 +1,15 @@
+import { ProductsTypeormRepository } from "./product-typeorm.repository";
+
+describe('ProductsTypeormRepository integrations tests', () => {
+  let productsRepository: ProductsTypeormRepository
+
+  beforeEach(() => {
+    
+  })
+
+  describe('method', () => {
+    it('testing', () => {
+    
+    })
+  })
+});
