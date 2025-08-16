ebec27248523ba12a883a2a2824ec513b7ad5507
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-16 17:26:43 -0300

    conexão com o banco de dados

--- src/products/infrastructure/typeorm/respositories/product-typeorm.repository.int-spec.ts
+++ src/products/infrastructure/typeorm/respositories/product-typeorm.repository.int-spec.ts
@@ -1,7 +1,16 @@
+import { testDataSource } from "@/common/infrastructure/typeorm/typeorm/testing/data-source";
 import { ProductsTypeormRepository } from "./product-typeorm.repository";
 
 describe('ProductsTypeormRepository integrations tests', () => {
   let productsRepository: ProductsTypeormRepository
+
+  beforeAll(async() => {
+     await testDataSource.initialize() 
+  })
+
+  afterAll(() => {
+
+  })
 
   beforeEach(() => {
     
