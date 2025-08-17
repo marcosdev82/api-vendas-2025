9eba96c1458dcbd1d47812985c2746f0cac85c0f
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-17 11:16:51 -0300

    Criação produto e pesquisa por id

--- src/products/infrastructure/typeorm/respositories/product-typeorm.repository.int-spec.ts
+++ src/products/infrastructure/typeorm/respositories/product-typeorm.repository.int-spec.ts
@@ -4,6 +4,7 @@ import { Product } from "../entities/products.entity";
 import { Not } from "typeorm";
 import { NotFoundError } from "@/common/domain/errors/not-found-error";
 import { randomUUID } from "crypto";
+import { ProductDataBuilder } from "../../in-memory/testing/helpers/products-data-builder";
 
 describe('ProductsTypeormRepository integrations tests', () => {
   let ormRepository: ProductsTypeormRepository
@@ -23,11 +24,21 @@ describe('ProductsTypeormRepository integrations tests', () => {
   })
 
   describe('findById', () => {
-    it('should gerenerate an error when the product is not found', async () => {
+    it('should generate an error when the product is not found', async () => {
       const id = randomUUID()
       await expect(ormRepository.findById(id)).rejects.toThrow(
         new NotFoundError(`Product not found using ID ${id}`),
       )
+    })
+
+    it('should finds a product by id', async () => {
+      const data = ProductDataBuilder({})
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      const result = await ormRepository.findById(product.id)
+      expect(result.id).toEqual(product.id)
+      expect(result.name).toEqual(product.name)
     })
   })
 });
